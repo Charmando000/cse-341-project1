2b28e8310d051bdf6c6cd4df6b3ff1a6449c1bcc
Charmando000 <elescogido0310@gmail.com>
2026-05-11 17:16:16 -0300

    Fix getAll to return all users

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,10 @@ const mongodb = require('../data/database');
 const objectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const results = await mongodb.getDb().db().collection('users').find({ _id: new objectId(req.params.id) });
+    const results = await mongodb.getDb().db().collection('users').find({});
     results.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
+        res.status(200).json(users);
     } );
 }
 
